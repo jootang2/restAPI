65a0b8d33b8bca20245ce23c50a3a466c51765b0
jootang2 <joohwan.song15b@gmail.com>
2022-11-28 01:05:24 +0900

    :tada: project creation

---
+++ src/main/java/me/jootang2/inflearn2/Inflearn2Application.java
@@ -0,0 +1,13 @@
+package me.jootang2.inflearn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inflearn2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Inflearn2Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/jootang2/inflearn2/Inflearn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jootang2.inflearn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inflearn2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
