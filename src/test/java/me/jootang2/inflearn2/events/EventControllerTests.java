7d3a7b2d99c0d166ae7e712874ab847d284dc7e4
jootang2 <joohwan.song15b@gmail.com>
2022-12-04 23:31:16 +0900

    :white_check_mark: Test : fail on unknown properties

--- src/test/java/me/jootang2/inflearn2/events/EventControllerTests.java
+++ src/test/java/me/jootang2/inflearn2/events/EventControllerTests.java
@@ -31,6 +31,34 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 11, 28, 22, 50))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 11, 29, 22, 50))
+                .beginEventDateTime(LocalDateTime.of(2022, 11, 30, 22, 50))
+                .endEventDateTime(LocalDateTime.of(2022, 12, 01, 22, 50))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("부평역 분수대 앞")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+        ;
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
@@ -52,13 +80,7 @@ public class EventControllerTests {
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
-
-
+                .andExpect(status().isBadRequest())
         ;
     }
 
