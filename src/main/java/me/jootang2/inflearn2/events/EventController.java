106023986ea5672d320ab660ddd268ca1c894a8c
jootang2 <joohwan.song15b@gmail.com>
2022-12-07 21:36:26 +0900

    :sparkles: build docs/index.html

--- src/main/java/me/jootang2/inflearn2/events/EventController.java
+++ src/main/java/me/jootang2/inflearn2/events/EventController.java
@@ -3,6 +3,7 @@ package me.jootang2.inflearn2.events;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -45,6 +46,7 @@ public class EventController {
         EventResource eventResource = new EventResource(event);
         eventResource.add(linkTo(EventController.class).withRel("query-events"));
         eventResource.add(selfLinkBuilder.withRel("update-event"));
+        eventResource.add(Link.of("/docs/index.html#resource-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
     }
 }
--- src/test/java/me/jootang2/inflearn2/events/EventControllerTests.java
+++ src/test/java/me/jootang2/inflearn2/events/EventControllerTests.java
@@ -74,7 +74,8 @@ public class EventControllerTests {
                         links(
                                 linkWithRel("self").description("link to self"),
                                 linkWithRel("query-events").description("link to query events"),
-                                linkWithRel("update-event").description("link to update an existing")
+                                linkWithRel("update-event").description("link to update an existing"),
+                                linkWithRel("profile").description("link to profile")
                         ),
                         requestHeaders(
                                 headerWithName(HttpHeaders.ACCEPT).description("accept header"),
@@ -113,8 +114,9 @@ public class EventControllerTests {
                                 fieldWithPath("eventStatus").description("eventStatus"),
                                 fieldWithPath("_links.self.href").description("link to self"),
                                 fieldWithPath("_links.query-events.href").description("link to query event list"),
-                                fieldWithPath("_links.update-event.href").description("link to update event")
-                                )
+                                fieldWithPath("_links.update-event.href").description("link to update event"),
+                                fieldWithPath("_links.profile.href").description("link to profile")
+                        )
                 ))
         ;
 
