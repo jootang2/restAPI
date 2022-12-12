388860dab7b21ed2b24f2e4e186205399dbd627c
jootang2 <joohwan.song15b@gmail.com>
2022-12-12 13:52:05 +0900

    :sparkles: GET /api/events(기존의 이벤트를 하나 조회)

--- src/main/java/me/jootang2/inflearn2/events/EventController.java
+++ src/main/java/me/jootang2/inflearn2/events/EventController.java
@@ -11,12 +11,11 @@ import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.net.URI;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
@@ -65,6 +64,19 @@ public class EventController {
 
     private ResponseEntity badRequest(Errors errors){
         return ResponseEntity.badRequest().body(new ErrorsResource(errors));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getEvent(@PathVariable Integer id){
+        Optional<Event> optionalEvent = eventRepository.findById(id);
+        if(optionalEvent.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Event event = optionalEvent.get();
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(Link.of("/docs/index.html#resources-events-get").withRel("profile"));
+        return ResponseEntity.ok(eventResource);
     }
 
 
--- src/test/java/me/jootang2/inflearn2/events/EventControllerTests.java
+++ src/test/java/me/jootang2/inflearn2/events/EventControllerTests.java
@@ -218,13 +218,38 @@ public class EventControllerTests {
 
     }
 
-    private void generateEvents(int index) {
+    @Test
+    @DisplayName("기존의 이벤트를 하나 조회하기")
+    public void getEvent() throws Exception{
+        //Given
+        Event event = generateEvents(100);
+
+        //When & Then
+        mockMvc.perform(get("/api/events/{id}", event.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").exists())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.profile").exists())
+                .andDo(document("get-an-event"))
+                ;
+    }
+
+    @Test
+    @DisplayName("없는 이벤트를 조회했을 때 404 응답받기")
+    public void getEvent404() throws Exception{
+        mockMvc.perform(get("/api/events/11532"))
+                .andExpect(status().isNotFound())
+                ;
+    }
+
+    private Event generateEvents(int index) {
         Event event = Event.builder()
                 .name("event" + index)
                 .description("test event")
                 .build()
                 ;
-        eventRepository.save(event);
+        return eventRepository.save(event);
 
     }
 
